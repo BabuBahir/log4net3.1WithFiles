72042781d2e4ac270edf37a53de42fa66f713195
Rohit <ofd.aws96@gmail.com>
2021-02-10 18:15:12 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace threePoint1Log4net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
