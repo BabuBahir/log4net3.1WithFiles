eff65c376279db77d3267f195bad0cf883e6c556
Rohit <ofd.aws96@gmail.com>
2021-02-10 18:35:55 +0530

    added myapp.log

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
 
 namespace threePoint1Log4net
 {
     class Program
     {
-        static void Main(string[] args)
-        { 
-            Console.WriteLine("Hello World!");
-        }
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)	       
+        {	        
+            Console.WriteLine("Hello World!");	            // Load configuration
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            Console.WriteLine("Hello world!");
+
+            // Log some things
+            log.Info("Hello logging world!");
+            log.Error("Error!");
+            log.Warn("Warn!");
+
+            Console.ReadLine();
+        }	        
     }
 }
